f0b4cd621cf9a38317de3c2467855b20955eb975
jamwest <jptwest@gmail.com>
2022-04-07 08:13:47 +1200

    wip

--- keyboards/planck/keymaps/CloseButNoCGar/keymap.c
+++ keyboards/planck/keymaps/CloseButNoCGar/keymap.c
@@ -23,6 +23,7 @@
 #endif
 
 enum planck_layers {
+  _ANISBVHTRO,
   _QWERTY,
   _LOWER,
   _RAISE,
@@ -37,32 +38,46 @@ enum planck_keycodes {
   BACKLIT
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-#define SPCSFT MT(MOD_LSFT, KC_SPC)
-#define ENTALT MT(MOD_LALT, KC_ENT)
-#define BKDEL SHIFT_NO(KC_BSPC, KC_DEL)
+#define LOW_DOT LT(_LOWER, KC_DOT)
+#define UP_COMM LT(_RAISE, KC_COMM)
+#define SPC_TAB SHIFT_NO(KC_SPC, KC_TAB)
+#define ENT_ESC SHIFT_NO(KC_ENT, KC_ESC)
+#define BK_DEL SHIFT_NO(KC_BSPC, KC_DEL)
+#define HOME_A LCTL_T(KC_A)
+#define HOME_N LALT_T(KC_N)
+#define HOME_I LGUI_T(KC_I)
+#define HOME_S LSFT_T(KC_S)
+#define HOME_H RSFT_T(KC_H)
+#define HOME_T RGUI_T(KC_T)
+#define HOME_R LALT_T(KC_R)
+#define HOME_O RCTL_T(KC_O)
 
+const uint16_t PROGMEM q_combo[] = {KC_O, KC_X, COMBO_END};
+const uint16_t PROGMEM z_combo[] = {KC_A, KC_UNDS, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(q_combo, KC_Q),
+    COMBO(z_combo, KC_Z),
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |   Q  |   W  |   E  |   R  |   T  | TAB  |Bs    |   Y  |   U  |   I  |   O  |   P  |
+ * |      |   P  |   U  |   C  |   J  |      |      |   K  |   L  |   D  |   W  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  | ESC  |  '   |   H  |   J  |   K  |   L  |   ;  |
+ * |   _  |   N  |   I  |   S  |   B  |      |      |   V  |   H  |   T  |   R  |   X  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   Z  |   X  |   C  |   V  |   B  | CTRL |  GUI |   N  |   M  |   ,  |   .  |   /  |
+ * |   A  |      |   Y  |   F  |      |  BS  |  Ent |      |   M  |   G  |      |   O  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Backli| MMode| MTog | Mouse|Lower | Space/Shift |Raise |En/Alt| Vol+ | Vol- | Mute |
+ * |      |      |      |   .  |   E  |             |  Spc |   ,  |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT_planck_grid(
+[_ANISBVHTRO] = LAYOUT_planck_grid(
 
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_TAB,  KC_BSPC,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,  KC_QUOT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LCTL, KC_LGUI,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-  BACKLIT, MU_MOD,  MU_TOG,  MOUSE,   LOWER,   SPCSFT,  SPCSFT,   RAISE,   ENTALT,  KC_VOLU, KC_VOLD, KC_MUTE
+  KC_NO,    KC_P,     KC_U,     KC_C,     KC_J,     KC_NO,    KC_NO,    KC_K,     KC_L,     KC_D,     KC_W,     KC_NO,
+  KC_UNDS,  HOME_N,   HOME_I,   HOME_S,   KC_B,     KC_NO,    KC_NO,    KC_V,     HOME_H,   HOME_T,   HOME_R,   KC_X,
+  HOME_A,   KC_NO,    KC_Y,     KC_F,     KC_NO,    BK_DEL,   ENT_ESC,  KC_NO,    KC_M,     KC_G,     KC_NO,    HOME_O,
+  KC_NO,    KC_NO,    KC_NO,    LOW_DOT,  KC_E,     KC_NO,    KC_NO,    SPC_TAB,  UP_COMM,  KC_NO,    KC_NO,    KC_NO
 ),
 
 /* Lower
--- keyboards/planck/keymaps/CloseButNoCGar/rules.mk
+++ keyboards/planck/keymaps/CloseButNoCGar/rules.mk
@@ -1,5 +1,5 @@
 SRC += muse.c
 AUDIO_ENABLE = yes
-
+COMBO_ENABLE = yes
 # Bootloader selection
 BOOTLOADER = stm32-dfu
